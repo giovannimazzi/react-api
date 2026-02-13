60bb6a5e6a42dfaecf78b4427c1b90c6a47f7ca5
giovannimazzi <gioelor@libero.it>
2026-02-13 17:05:09 +0100

    MILESTONE 1 raggiunta

--- README.md
+++ README.md
@@ -1,5 +1,48 @@
-# Assegnazione del gg/mm/20aa
+# Assegnazione del 13/02/2026
 
-**TITLE**
+**Cast Fetching**
 
-nome repo: `repo-name`
+nome repo: `react-api`
+
+**Esercizio**
+
+E’ arrivato il momento di mettere insieme i concetti appresi creiamo una piccola app che ci mostri un elenco di attori o attrici.
+
+**Usate uno di questi due endpoint, a piacimento:**
+
+Lista di Attrici: https://lanciweb.github.io/demo/api/actresses/
+
+Lista di Attori: https://lanciweb.github.io/demo/api/actors/
+
+**MILESTONE 1**\
+Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.
+
+**MILESTONE 2**\
+Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
+
+- nome
+- anno nascita
+- nazionalità
+- biografia
+- immagine
+- riconoscimenti
+
+**MILESTONE 3**\
+Mostriamo in pagina una card per ciascun attore, con grafica a piacimento!
+
+**BONUS 1 ☺️**\
+Stampare sia una lista delle attrici che degli attori, separatamente.
+
+**BONUS 2 😎**\
+Stampare un’unica lista che contiene attori e attrici insieme!
+
+**BONUS 3** 🤯\
+Aggiungere nella card dell’attore/attrice i film più famosi
+
+> 🪄
+>
+> _Se questa task è troppo difficile, prova ad aiutarti con l’AI!_
+>
+> _Cerca però sempre di comprendere quello che ti viene suggerito 😉_
+
+Buon lavoro!
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,60 @@
-/* import axios from "axios"; */
-import { GiPowerButton } from "react-icons/gi";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const acrtessesApi = "https://lanciweb.github.io/demo/api/actresses/";
+const actorsApi = "https://lanciweb.github.io/demo/api/actors/";
 
 export default function App() {
+  /*  const [dataList, setDataList] = useState([]);
+   */
+  const getApiData = (ApiUrl) => {
+    axios
+      .get(ApiUrl)
+      .then((res) => console.log(res.data))
+      .catch((e) => {
+        alert("API ERROR: " + e.message);
+      });
+  };
+
+  useEffect(() => {
+    getApiData(acrtessesApi);
+  }, []);
+
   return (
-    <div className="p-3">
-      <h1>HELLO WORLD!!!</h1>
-      <button className="btn btn-primary d-flex align-items-center gap-2">
-        <GiPowerButton className="text-warning fw-bold" />
-        BUTTON
-      </button>
+    <div id="app" className="container-fluid p-0 h-100 d-flex flex-column">
+      <header className="text-center ">
+        <h1 className="text-primary mb-0">CAST FETCHING</h1>
+      </header>
+      <main className="flex-grow-1">
+        <div className="container py-4">
+          <div className="row row-cols-2">
+            <div className="col">
+              <div className="card mb-3">
+                <div className="row g-0">
+                  <div className="col-md-4">
+                    {/* <img src="..." className="img-fluid rounded-start" alt="..."> */}
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">Card title</h5>
+                      <p className="card-text">
+                        This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer.
+                      </p>
+                      <p className="card-text">
+                        <small className="text-body-secondary">
+                          Last updated 3 mins ago
+                        </small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
