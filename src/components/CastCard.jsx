01f17faaaaa0c680c6ed2b6b5af112838a5bef0d
giovannimazzi <gioelor@libero.it>
2026-02-13 20:31:31 +0100

    Inserite Bandierine

--- src/components/CastCard.jsx
+++ src/components/CastCard.jsx
@@ -1,3 +1,23 @@
+const nationalityFlags = [
+  { nationality: "American", flag: "us" },
+  { nationality: "British", flag: "gb" },
+  { nationality: "Australian", flag: "au" },
+  { nationality: "Israeli-American", flag: ["il", "us"] },
+  { nationality: "South African", flag: "za" },
+  { nationality: "French", flag: "fr" },
+  { nationality: "Indian", flag: "in" },
+  { nationality: "Israeli", flag: "il" },
+  { nationality: "Spanish", flag: "es" },
+  { nationality: "South Korean", flag: "kr" },
+  { nationality: "Chinese", flag: "cn" },
+  { nationality: "Scottish", flag: "gb-sct" },
+  { nationality: "New Zealand", flag: "nz" },
+  { nationality: "Hong Kong", flag: "hk" },
+  { nationality: "German", flag: "de" },
+  { nationality: "Canadian", flag: "ca" },
+  { nationality: "Irish", flag: "ie" },
+];
+
 export default function CastCard({ item }) {
   const movies = item.most_famous_movies ?? item.known_for ?? [];
   return (
@@ -14,9 +34,31 @@ export default function CastCard({ item }) {
           <div className="col-md-8">
             <div className="card-body">
               <h5 className="card-title">{item.name}</h5>
-              <h6 className="card-subtitle mb-2 text-body-secondary">
-                {item.birth_year} - {item.nationality}
+
+              <h6 className="card-subtitle mb-2 text-body-secondary d-flex align-items-center">
+                {item.birth_year} - {item.nationality}{" "}
+                {item.nationality.includes("-") ? (
+                  <>
+                    <img
+                      src={`https://flagcdn.com/28x21/${nationalityFlags.find((n) => n.nationality === item.nationality).flag[0]}.png`}
+                      alt="flag"
+                      className="ms-2 "
+                    />
+                    <img
+                      src={`https://flagcdn.com/28x21/${nationalityFlags.find((n) => n.nationality === item.nationality).flag[1]}.png`}
+                      alt="flag"
+                      className="ms-2 "
+                    />
+                  </>
+                ) : (
+                  <img
+                    src={`https://flagcdn.com/28x21/${nationalityFlags.find((n) => n.nationality === item.nationality).flag}.png`}
+                    alt="flag"
+                    className="ms-2 "
+                  />
+                )}
               </h6>
+
               <p className="card-text">{item.biography}</p>
               <div className="card-text mb-2">
                 <strong className="text-muted">Awards: </strong>
